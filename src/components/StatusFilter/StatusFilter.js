06078d34d9ab741f9f4074cf28a2a174c53bebfa
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-10-30 17:33:22 +0200

    end

--- src/components/StatusFilter/StatusFilter.js
+++ src/components/StatusFilter/StatusFilter.js
@@ -1,16 +1,29 @@
 import { Button } from "components/Button/Button";
 import css from "./StatusFilter.module.css";
 import { getStatusFilter } from "redux/selectors";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { setStatusFilter } from "redux/actions";
 import { statusFilters } from "redux/constants";
 
 export const StatusFilter = () => {
+  const dispatch = useDispatch();
   const filter = useSelector(getStatusFilter)
+  const handleFilterChenge = filter => dispatch(setStatusFilter(filter))
+  
   return (
     <div className={css.wrapper}>
-      <Button selected={filter === statusFilters.all}>All</Button>
-      <Button selected={filter === statusFilters.active}>Active</Button>
-      <Button selected={filter === statusFilters.completed}>Completed</Button>
+      <Button selected={filter === statusFilters.all}
+        onClick={() => handleFilterChenge(statusFilters.all)}
+      >All
+      </Button>
+      <Button selected={filter === statusFilters.active}
+        onClick={() => handleFilterChenge(statusFilters.active)}
+      >Active
+      </Button>
+      <Button selected={filter === statusFilters.completed}
+        onClick={() => handleFilterChenge(statusFilters.completed)}
+      >Completed
+      </Button>
     </div>
   );
 };
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,16 +1,22 @@
 import { MdClose } from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { deleteTask, toggleCompleted } from "redux/actions";
 import css from "./Task.module.css";
 
 export const Task = ({ task }) => {
+  const dispatch = useDispatch();
+  const handleDelete = () => dispatch(deleteTask(task.id))
+  const handleToggle = () => dispatch(toggleCompleted(task.id))
   return (
     <div className={css.wrapper}>
       <input
         type="checkbox"
         className={css.checkbox}
+        onChange={handleToggle}
         checked={task.completed}
       />
       <p className={css.text}>{task.text}</p>
-      <button className={css.btn}>
+      <button className={css.btn} onClick={handleDelete}>
         <MdClose size={24} />
       </button>
     </div>
